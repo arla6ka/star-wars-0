7c1fff0b7b2d5a6078c7269b1ada89268c3b8e05
arla6ka <154040187+arla6ka@users.noreply.github.com>
2024-06-01 23:45:20 +0500

    Update Planets.js

--- client/src/components/Planets.js
+++ client/src/components/Planets.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-axios.defaults.withCredentials = true;
+
 const Planets = () => {
   const [planets, setPlanets] = useState([]);
   const [loading, setLoading] = useState(true);
